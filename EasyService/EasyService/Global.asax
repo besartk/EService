48b2fb5e7a367f25400bc9614cfff6e9b64e3512
bbesartk <besart.k@trokit.com>
2017-09-10 12:55:20 +0200

    mernnii

---
+++ EasyService/EasyService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EasyService.MvcApplication" Language="C#" %>
---
+++ EasyService/EasyService/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EasyService.Startup))]
+namespace EasyService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
